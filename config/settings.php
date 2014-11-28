55d566697568f54618752685f265f93ed656c1e0
Aaron Schachter <aaronschachter@gmail.com>
2014-11-28 17:49:50 -0500

    Working makefile

---
+++ config/settings.php
@@ -0,0 +1,19 @@
+<?php
+
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'aaronschachter',
+      'username' => 'root',
+      'password' => 'root',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
+
+$conf['stage_file_proxy_origin'] = 'http://www.aaronschachter.com'; 
